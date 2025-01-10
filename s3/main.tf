f4848945c0efd72e2abce63749f70daae730ba7a
mikemakin <tomiogunmakin@gmail.com>
2025-01-10 17:44:19 -0500

    lifecycle and versioning config

--- s3/main.tf
+++ s3/main.tf
@@ -2,9 +2,24 @@ resource "aws_s3_bucket" "s3_bucket" {
     bucket = var.bucket_name
 
     tags   = var.tags
+}
 
-    lifecycle_rule {
-        enabled = true
-        abort_incomplete_multipart_upload_days = 7
+resource "aws_s3_bucket_lifecycle_configuration" "lifecycle_config" {
+  bucket = aws_s3_bucket.s3_bucket.id
+
+  rule {
+    id = "bucket-rule"
+
+    status = "Enabled"
+    abort_incomplete_multipart_upload {
+        days_after_initiation = 7
     }
+  }
+}
+
+resource "aws_s3_bucket_versioning" "versioning_config" {
+  bucket = aws_s3_bucket.s3_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
 }
